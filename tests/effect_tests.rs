558b5d1c31e840693ddfbd051837ab989cbe3b6c
Katie Kloss <katherine.kloss@proton.me>
2026-04-04 17:28:04 -0400

    add Strength test

--- tests/effect_tests.rs
+++ tests/effect_tests.rs
@@ -7,12 +7,33 @@ fn start_run() -> Run {
         deck: vec![],
         floor: 1,
         gold: 0,
-        health: 1,
+        health: 70,
         relics: vec![],
         current_room: MapGenerator::generate()
     };
 
     run
+}
+
+#[test]
+pub fn applying_strength_increases_damage() {
+    let mut run = start_run();
+    run.deck.push(CardInstance::new(Card::SilentStrike));
+    run.deck.push(CardInstance::new(Card::SilentStrike));
+
+    let mut encounter = Encounter::new(&run);
+    encounter.enemies.push(Enemy::new(Monsters::FuzzyWurmCrawler));
+    assert_eq!(encounter.enemies[0].health, 55);
+
+    encounter.begin_turn();
+
+    encounter.player.effects.push(spire_rs::Effect::Strength(1));
+    encounter.play_by_id_with_target(get_card!(Card::SilentStrike, encounter.hand).unwrap().id, encounter.enemies[0].id);
+    assert_eq!(encounter.enemies[0].health, 48);
+
+    encounter.enemies[0].effects.push(spire_rs::Effect::Strength(1));
+    encounter.yield_turn();
+    assert_eq!(encounter.player.health, 65);
 }
 
 #[test]
@@ -24,13 +45,14 @@ pub fn applying_weak_reduces_damage() {
     let mut encounter = Encounter::new(&run);
     encounter.enemies.push(Enemy::new(Monsters::FuzzyWurmCrawler));
     assert_eq!(encounter.enemies[0].health, 55);
-    
-    encounter.begin_turn();
 
-    encounter.play_by_id_with_target(get_card!(Card::SilentStrike, encounter.hand).unwrap().id, encounter.enemies[0].id);
-    assert_eq!(encounter.enemies[0].health, 49);
+    encounter.begin_turn();
 
     encounter.player.effects.push(spire_rs::Effect::Weak(1));
     encounter.play_by_id_with_target(get_card!(Card::SilentStrike, encounter.hand).unwrap().id, encounter.enemies[0].id);
-    assert_eq!(encounter.enemies[0].health, 45);
+    assert_eq!(encounter.enemies[0].health, 51);
+
+    encounter.enemies[0].effects.push(spire_rs::Effect::Weak(1));
+    encounter.yield_turn();
+    assert_eq!(encounter.player.health, 67);
 }
