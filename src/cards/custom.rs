b1fd026530efa98ade1f9464c49af9a34b0ff180
Katie Kloss <katherine.kloss@proton.me>
2026-04-07 19:21:09 -0400

    fix Ricochet hitting dead enemies

--- src/cards/custom.rs
+++ src/cards/custom.rs
@@ -1,16 +1,19 @@
 use rand::RngExt;
 
-use crate::{cards::CustomCard, encounters::Encounter};
+use crate::{cards::CustomCard, encounters::Encounter, monsters::Enemy};
 
 pub static RICOCHET: CustomCard = CustomCard {
     play: Some(|_, encounter| {
         let mut rng = rand::rng();
-        let enemy_count = encounter.enemies.len();
         
         for _ in 0..4 {
-            // TODO: make sure they're alive
-            let target= &mut encounter.enemies[rng.random_range(0..enemy_count)];
-            Encounter::resolve_attack(target, 3);
+            let mut alive: Vec<&mut Enemy> = encounter.enemies.iter_mut().filter(|e| e.health > 0).collect();
+            let n = alive.len();
+            if n == 0 {
+                break;
+            }
+            
+            Encounter::resolve_attack(alive[rng.random_range(..n)], 3);
         }
     })
 };
