9b4e98b622577882ed03ec26814f096b308d6280
Katie Kloss <katherine.kloss@proton.me>
2026-04-15 09:29:44 -0400

    more sketching

--- src/bin/mcts.rs
+++ src/bin/mcts.rs
@@ -1,18 +1,20 @@
-use std::collections::HashMap;
+use std::{cell::{Cell, UnsafeCell}, collections::HashMap, sync::{LazyLock, Mutex, RwLock}};
 
 use rand::{RngExt, rngs::ThreadRng};
 use spire_rs::{EncounterOp, Run, cards::{CardInstance, library::Card}, core::Encounter, monsters::{Enemy, Monsters}};
 
-static EXPLORE_RATE: f64 = 1.;
 const EXPLORE_DECAY: f64 = 0.99999975;
+
+static EXPLORE_RATE: Mutex<f64> = Mutex::new(1.);
+static MEMORY: LazyLock<Memory> = LazyLock::new(|| Memory { states: HashMap::new()});
 
 fn start_encounter() -> Encounter {
     let mut run = Run {
         floor: 0,
         relics: HashMap::new(),
-        health: 70,
+        health: 5,
         gold: 99,
-        max_health: 70,
+        max_health: 5,
         deck: vec![],
     };
 
@@ -31,29 +33,94 @@ fn start_encounter() -> Encounter {
 
 fn main() -> std::io::Result<()> {
     let mut rng = rand::rng();
-    let mut mem = Memory {
-
-    };
-
-    loop {
-        let encounter = start_encounter();
+    
+    for i in 0..10 {
+        let mut encounter = start_encounter();
+        encounter.begin_turn();
+        play_from(&mut rng, encounter, Order::Evaluate);
         
-        while encounter.player.health > 0 && encounter.enemies[0].health > 0 {
-           
-        }
+        let mut rate = EXPLORE_RATE.lock().unwrap();
+        *rate = *rate * EXPLORE_DECAY;
     }
 
     Ok(())
 }
 
-pub fn play_from(rng: &mut ThreadRng, mut encounter: Encounter, position: &Position) {
+// returns (successful_position, won)
+pub fn play_from(rng: &mut ThreadRng, mut encounter: Encounter, order: Order) -> PlayResult {
+    if encounter.player.health == 0 {
+        return PlayResult::Lost;
+    } else if encounter.enemies[0].health == 0 {
+        return PlayResult::Won;
+    }
+
+    if encounter.player.energy == 0 {
+        encounter.yield_turn();
+
+        if encounter.player.health == 0 {
+            return PlayResult::Lost;
+        } else if encounter.enemies[0].health == 0 {
+            return PlayResult::Won;
+        }
+
+        encounter.end_turn();
+        encounter.begin_turn();
+        println!("Turn {}", encounter.turn);
+    }
+
+    let position = get_position(&encounter);
+
     let actions = get_all_actions(&encounter);
-    if rng.random_range(0. .. 1.) < EXPLORE_RATE {
-        let our_action = &actions[rng.random_range(0..actions.len())];
+    let our_action = {
+        let explore_rate = EXPLORE_RATE.lock().unwrap();
+
+        if matches!(order, Order::Playout) || rng.random_range(0. .. 1.) < *explore_rate {
+            &actions[rng.random_range(0..actions.len())]
+        } else {
+            recall_closest_position(&position)
+        }
+    };
+
+    let mut encounter = encounter.clone();
+    match our_action {
+        Action::PlayAgainst(card) => encounter.play(encounter.hand.iter().filter(|c| c.card == *card).nth(0).unwrap().id, encounter.enemies[0].id, vec![], &vec![]),
+        Action::PlaySelf(card) => encounter.play(encounter.hand.iter().filter(|c| c.card == *card).nth(0).unwrap().id, 0, vec![], &vec![])
+    };
+
+    if encounter.enemies[0].health == 0 {
+        return PlayResult::Won; // hmm.
+    }
+
+    let this_position = get_position(&encounter);
+
+    if let Order::Evaluate = order {
+        let mut wins = 0;
+        for i in 1..5 {
+            println!("Eval iteration {} from {:?}", i, &this_position);
+            let new_encounter = encounter.clone();
+            let result = play_from(rng, new_encounter, Order::Playout);
+
+            match result {
+                PlayResult::Failure | PlayResult::Lost => println!("Died in iteration {} from {:?}", i, &this_position),
+                PlayResult::Success | PlayResult::Won => {
+                    wins += 1;
+                    println!("Won in iteration {} from {:?}", i, &this_position);
+                }
+            }
+        }
+
+        if wins > 2 {
+            return PlayResult::Success;
+        } else {
+            return PlayResult::Failure;
+        }
+    } else {
+        println!("Continuing play out from {:?}", this_position);
+        return play_from(rng, encounter, Order::Playout);
     }
 }
 
-fn get_position(encounter: &mut Encounter) -> Position {
+fn get_position(encounter: &Encounter) -> Position {
     let incoming_damage = match encounter.get_enemy_intent(&encounter.enemies[0])[0] {
         EncounterOp::AttackPlayer(_, dmg) => Encounter::query_attack_damage(&encounter.enemies[0], &encounter.player, dmg),
         EncounterOp::Damage(_, dmg) => dmg,
@@ -82,15 +149,35 @@ fn get_all_actions(encounter: &Encounter) -> Vec<Action> {
     actions
 }
 
+fn recall_closest_position(position: &Position) -> &Action {
+    &MEMORY.states.get(position).expect("Unknown position")
+}
+
 pub struct Memory {
     pub states: HashMap<Position, Action>
 }
 
+#[derive(Debug)]
 pub enum Action {
     PlaySelf(Card),
     PlayAgainst(Card)
 }
 
+#[derive(Clone, Debug)]
+pub enum Order {
+    Evaluate,
+    Playout
+}
+
+#[derive(Clone, Debug)]
+pub enum PlayResult {
+    Won,
+    Lost,
+    Success,
+    Failure
+}
+
+#[derive(Hash, PartialEq, Eq, Debug)]
 pub struct Position {
     pub hand: Vec<Card>,
     pub incoming_damage: u32,
